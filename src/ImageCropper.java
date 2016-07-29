d3c5d10fd48dbafa39fbe4e2e94c851b21caf2da
Tobias Schürg <tobiasschuerg@gmail.com>
2016-07-29 10:33:55 +0200

    script now processes the whole folder

---
+++ src/ImageCropper.java
@@ -0,0 +1,135 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Cropps an image to its bounding box.
+ * <p>
+ * Created by Tobias on 29.07.2016.
+ */
+class ImageCropper {
+
+    private int border = 1;
+    private int skipped = 0;
+
+    /**
+     * @param border additional background padding to preserver color.
+     */
+    public ImageCropper(int border) {
+        this.border = border;
+    }
+
+
+    public void crop(File f) {
+
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(f);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        int topCrop = 0;
+        int color = img.getRGB(0, 0);
+        for (int y = 0; y < img.getHeight(); y++) {
+            boolean removableLine = true;
+            for (int x = 0; x < img.getWidth(); x++) {
+                int c = img.getRGB(x, y);
+                if (c != color) {
+                    removableLine = false;
+                    break;
+                }
+            }
+            if (removableLine) {
+                topCrop++;
+            } else {
+                System.out.print(" - t: " + topCrop);
+                break;
+            }
+        }
+
+        int leftCrop = 0;
+        for (int x = 0; x < img.getWidth(); x++) {
+            boolean removableLine = true;
+            for (int y = 0; y < img.getHeight(); y++) {
+                int c = img.getRGB(x, y);
+                if (c != color) {
+                    removableLine = false;
+                    break;
+                }
+            }
+            if (removableLine) {
+                leftCrop++;
+            } else {
+                System.out.print("l: " + leftCrop);
+                break;
+            }
+        }
+
+        int bottomCrop = 0;
+        for (int y = img.getHeight() - 1; y > 0; y--) {
+            boolean removableLine = true;
+            for (int x = 0; x < img.getWidth(); x++) {
+                int c = img.getRGB(x, y);
+                if (c != color) {
+                    removableLine = false;
+                    break;
+                }
+            }
+            if (removableLine) {
+                bottomCrop++;
+            } else {
+                System.out.print(", b: " + bottomCrop);
+                break;
+            }
+        }
+
+        int rightCrop = 0;
+        for (int x = 0; x < img.getWidth(); x++) {
+            boolean removableLine = true;
+            for (int y = 0; y < img.getHeight(); y++) {
+                int c = img.getRGB(x, y);
+                if (c != color) {
+                    removableLine = false;
+                    break;
+                }
+            }
+            if (removableLine) {
+                rightCrop++;
+            } else {
+                System.out.println(", r: " + rightCrop);
+                break;
+            }
+        }
+
+        topCrop -= border;
+        topCrop = topCrop < 0 ? 0 : topCrop;
+
+        bottomCrop -= border;
+        bottomCrop = bottomCrop < 0 ? 0 : bottomCrop;
+
+        leftCrop -= border;
+        leftCrop = leftCrop < 0 ? 0 : leftCrop;
+
+        rightCrop -= border;
+        rightCrop = rightCrop < 0 ? 0 : rightCrop;
+
+        if (img.getHeight() - topCrop - bottomCrop < 1) {
+            skipped++;
+            return;
+        }
+
+        BufferedImage SubImgage = img.getSubimage(leftCrop, topCrop, img.getWidth() - leftCrop - rightCrop, img.getHeight() - topCrop - bottomCrop);
+
+        File outputfile = new File("cropped/" + f.getName());
+
+        try {
+            ImageIO.write(SubImgage, "png", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,104 +2,28 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        File f = new File("in/foo.png");
-
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(f);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        int topCrop = 0;
-        int color = img.getRGB(0, 0);
-        for (int y = 0; y < img.getHeight(); y++) {
-            boolean removableLine = true;
-            for (int x = 0; x < img.getWidth(); x++) {
-                int c = img.getRGB(x, y);
-                if (c != color) {
-                    removableLine = false;
-                    break;
-                }
-            }
-            if (removableLine) {
-                topCrop++;
-            } else {
-                System.out.println("topcrop " + topCrop);
-                break;
-            }
-        }
-
-        int leftCrop = 0;
-        for (int x = 0; x < img.getWidth(); x++) {
-            boolean removableLine = true;
-            for (int y = 0; y < img.getHeight(); y++) {
-                int c = img.getRGB(x, y);
-                if (c != color) {
-                    removableLine = false;
-                    break;
-                }
-            }
-            if (removableLine) {
-                leftCrop++;
-            } else {
-                System.out.println("leftcrop " + leftCrop);
-                break;
-            }
-        }
-
-        int bottomCrop = 0;
-        for (int y = img.getHeight() - 1; y > 0; y--) {
-            boolean removableLine = true;
-            for (int x = 0; x < img.getWidth(); x++) {
-                int c = img.getRGB(x, y);
-                if (c != color) {
-                    removableLine = false;
-                    break;
-                }
-            }
-            if (removableLine) {
-                bottomCrop++;
-            } else {
-                System.out.println("bottomCrop " + bottomCrop);
-                break;
-            }
-        }
-
-        int rightCrop = 0;
-        for (int x = 0; x < img.getWidth(); x++) {
-            boolean removableLine = true;
-            for (int y = 0; y < img.getHeight(); y++) {
-                int c = img.getRGB(x, y);
-                if (c != color) {
-                    removableLine = false;
-                    break;
-                }
-            }
-            if (removableLine) {
-                rightCrop++;
-            } else {
-                System.out.println("rightCrop " + rightCrop);
-                break;
-            }
-        }
-
-        topCrop--;
-        bottomCrop--;
-        leftCrop--;
-        rightCrop--;
-        BufferedImage SubImgage = img.getSubimage(leftCrop, topCrop, img.getWidth() - leftCrop - rightCrop, img.getHeight() - topCrop - bottomCrop);
-
-        File outputfile = new File("out/foo.png");
+    /**
+     * Cropps all images in the "in/" folder
+     */
+    public static void main(String[] ignored) {
+        Path path = Paths.get("in/");
+        System.out.println("Cropping images in: " + path);
+        ImageCropper cropper = new ImageCropper(3);
 
         try {
-            ImageIO.write(SubImgage, "png", outputfile);
+            final int[] count = {0};
+            Files.walk(path).forEach(pathConsumer -> {
+                if (Files.isRegularFile(pathConsumer)) {
+                    System.out.print(count[0]++ + ": processing: " + pathConsumer);
+                    cropper.crop(pathConsumer.toFile());
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
